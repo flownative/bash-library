2329d3c8e58fc1f2c1994e33508dc47dc899f554
Robert Lemke <robert@flownative.com>
2020-01-28 11:46:15 +0100

    Initial commit

---
+++ lib-log.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+# =======================================================================================
+# LIBRARY: LOG
+# =======================================================================================
+
+# Colors
+RESET='\033[0m'
+RED='\033[38;5;1m'
+GREEN='\033[38;5;2m'
+YELLOW='\033[38;5;3m'
+MAGENTA='\033[38;5;5m'
+
+# ---------------------------------------------------------------------------------------
+# stderr_print() - Print to STDERR
+#
+# @arg The message to print
+# @return void
+#
+stderr_print() {
+    printf "%b\\n" "${*}" >&2
+}
+
+# ---------------------------------------------------------------------------------------
+# debug() – Log a message with severity "debug"
+#
+# @arg The message to log
+# @return void
+#
+debug() {
+    local -r bool="${LOG_DEBUG:-false}"
+    shopt -s nocasematch
+    if [[ "$bool" == 1 || "$bool" =~ ^(yes|true)$ ]]; then
+        log "${MAGENTA}DEBUG${RESET} ${*}"
+    fi
+}
+
+# ---------------------------------------------------------------------------------------
+# log() - Log a message
+#
+# @arg The message to log
+# @return void
+#
+log() {
+    stderr_print "${MAGENTA}$(date "+%T.%2N ")${RESET}${*}"
+}
+
+# ---------------------------------------------------------------------------------------
+# info() - Log a message with severity "info"
+#
+# @arg The message to log
+# @return void
+#
+info() {
+    log "${GREEN}INFO ${RESET} ${*}"
+}
+
+# ---------------------------------------------------------------------------------------
+# warn() - Log a message with severity "warning"
+#
+# @arg The message to log
+# @return void
+#
+warn() {
+    log "${YELLOW}WARN ${RESET} ${*}"
+}
+
+# ---------------------------------------------------------------------------------------
+# error() - Log a message with severity "error"
+#
+# @arg The message to log
+# @return void
+#
+error() {
+    log "${RED}ERROR${RESET} ${*}"
+}
---
+++ lib-validation.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# shellcheck disable=SC1090
+
+# =======================================================================================
+# LIBRARY: VALIDATION
+# =======================================================================================
+
+# ---------------------------------------------------------------------------------------
+# is_boolean_yes() - Checks if the given argument is a bool or if it's "true" or "yes"
+#
+# @arg Value to check
+# @return bool
+#
+is_boolean_yes() {
+    local -r bool="${1:-}"
+    shopt -s nocasematch
+    if [[ "$bool" = 1 || "$bool" =~ ^(yes|true)$ ]]; then
+        true
+    else
+        false
+    fi
+}
