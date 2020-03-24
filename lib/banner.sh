fa142f7ef8fdc856b701c83d1e0da792ea366519
Robert Lemke <robert@flownative.com>
2020-03-24 17:15:13 +0100

    Rename banner_beach_instance to banner_generic

--- lib/banner.sh
+++ lib/banner.sh
@@ -24,19 +24,23 @@ banner_flownative() {
 }
 
 # ---------------------------------------------------------------------------------------
-# banner_beach_instance() - Display a Beach instance banner
+# banner_generic() - Display a generic banner
 #
+# @arg string The title
+# @arg string An optional description line
+# @arg string A second optional description line
 # @return void
 #
-banner_beach_instance() {
-    local -r instance_name="${1:?missing instance name}"
-    local -r instance_identifier="${2:-}"
+banner_generic() {
+    local -r title="${1:?missing title}"
+    local -r description_1="${2:-}"
+    local -r description_2="${3:-}"
 
     if [ "${BANNER_FLOWNATIVE_SKIP}" = "" ]; then
         echo '[38;5;255;255;255m        [0m[38;5;7m                                                                 [0m'
-        echo "[38;5;196;48;5;196m       [0m[38;5;255;255;255m   $(printf "%-61s" "Flownative Beach") [0m"
-        echo "[38;5;196;48;5;196m       [0m[38;5;7m   $(printf "%-61s" "${instance_name}") [0m"
-        echo "[38;5;196;48;5;196m       [0m[38;5;7m   $(printf "%-61s" "${instance_identifier}") [0m"
+        echo "[38;5;196;48;5;196m       [0m[38;5;255;255;255m   $(printf "%-61s" "${title}") [0m"
+        echo "[38;5;196;48;5;196m       [0m[38;5;7m   $(printf "%-61s" "${description_1}") [0m"
+        echo "[38;5;196;48;5;196m       [0m[38;5;7m   $(printf "%-61s" "${description_2}") [0m"
         echo '[38;5;255;255;255m        [0m[38;5;7m                                                                 [0m'
     fi
 }
