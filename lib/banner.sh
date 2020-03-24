591c0b92981542b0dd81d3ad9d433b58417eee63
Robert Lemke <robert@flownative.com>
2020-03-24 16:29:31 +0100

    Introduce banner_beach_instance()

--- lib/banner.sh
+++ lib/banner.sh
@@ -22,3 +22,21 @@ banner_flownative() {
         echo '[38;5;255;255;255m        [0m[38;5;7m                                             [0m'
     fi
 }
+
+# ---------------------------------------------------------------------------------------
+# banner_beach_instance() - Display a Beach instance banner
+#
+# @return void
+#
+banner_beach_instance() {
+    local -r instance_identifier="${1:?missing instance identifier}"
+    local -r instance_name="${1:?missing instance name}"
+
+    if [ "${BANNER_FLOWNATIVE_SKIP}" = "" ]; then
+        echo '[38;5;255;255;255m        [0m[38;5;7m                                             [0m'
+        echo "[38;5;196;48;5;196m       [0m[38;5;255;255;255m   $(printf "%-41s" "Flownative Beach") [0m"
+        echo "[38;5;196;48;5;196m       [0m[38;5;7m   $(printf "%-41s" "${instance_name}") [0m"
+        echo "[38;5;196;48;5;196m       [0m[38;5;7m   $(printf "%-41s" "${instance_identifier}") [0m"
+        echo '[38;5;255;255;255m        [0m[38;5;7m                                             [0m'
+    fi
+}
