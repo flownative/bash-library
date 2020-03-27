df40d6020dd4db3f315226c1ee29b6036afdff65
Robert Lemke <robert@flownative.com>
2020-03-27 17:14:22 +0100

    Introduce is_boolean_no()

--- lib/validation.sh
+++ lib/validation.sh
@@ -6,7 +6,7 @@
 # =======================================================================================
 
 # ---------------------------------------------------------------------------------------
-# is_boolean_yes() - Checks if the given argument is a bool or if it's "true" or "yes"
+# is_boolean_yes() - Checks if the given argument is a bool or if it's "true" or "yes" or "on"
 #
 # @arg Value to check
 # @return bool
@@ -15,6 +15,22 @@ is_boolean_yes() {
     local -r bool="${1:-}"
     shopt -s nocasematch
     if [[ "$bool" = 1 || "$bool" =~ ^(yes|true|on)$ ]]; then
+        true
+    else
+        false
+    fi
+}
+
+# ---------------------------------------------------------------------------------------
+# is_boolean_no() - Checks if the given argument is a bool false or if it's "false" or "no" or "off"
+#
+# @arg Value to check
+# @return bool
+#
+is_boolean_no() {
+    local -r bool="${1:-}"
+    shopt -s nocasematch
+    if [[ "$bool" = 0 || "$bool" =~ ^(no|false|off)$ ]]; then
         true
     else
         false
