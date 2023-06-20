5bfc2eb0091f5a32c4d54103336b52f1095fa036
Robert Lemke <robert@flownative.com>
2023-06-20 15:55:04 +0200

    Switch banner to Flownative again
    Of course, we still stand with Ukraine. But it's also important to show our logo again.

--- lib/banner.sh
+++ lib/banner.sh
@@ -21,11 +21,11 @@ banner_flownative() {
     local -r image_name="${1:?missing image name}"
 
     if [ "${BANNER_FLOWNATIVE_SKIP}" = "" ]; then
-        echo '[38;5;255;255;255m                          [0m[38;5;7m                                             [0m'
-        echo "[38;5;196;48;2;255;215;000m                 [0m[38;5;255;255;255m   $(printf "%-41s" "${image_name}") [0m"
-        echo '[38;5;196;48;2;000;087;183m                 [0m[38;5;7m   This Docker image was handcrafted for you [0m'
-        echo '[38;5;255;255;255m#standwithukraine [0m[38;5;7m  by Flownative.         www.flownative.com [0m'
-        echo '[38;5;255;255;255m                          [0m[38;5;7m                                             [0m'
+        echo '[38;5;255;255;255m        [0m[38;5;7m                                             [0m'
+        echo "[38;5;196;48;5;196m       [0m[38;5;255;255;255m   $(printf "%-41s" "${image_name}") [0m"
+        echo '[38;5;196;48;5;196m       [0m[38;5;7m   This Docker image was handcrafted for you [0m'
+        echo '[38;5;196;48;5;196m       [0m[38;5;7m   by Flownative.         www.flownative.com [0m'
+        echo '[38;5;255;255;255m        [0m[38;5;7m                                             [0m'
     fi
 }
 
@@ -43,10 +43,10 @@ banner_generic() {
     local -r description_2="${3:-}"
 
     if [ "${BANNER_FLOWNATIVE_SKIP}" = "" ]; then
-        echo '[38;5;255;255;255m                          [0m[38;5;7m                                             [0m'
-        echo "[38;5;196;48;2;255;215;000m                 [0m[38;5;255;255;255m   $(printf "%-61s" "${title}") [0m"
-        echo "[38;5;196;48;2;000;087;183m                 [0m[38;5;7m   $(printf "%-61s" "${description_1}") [0m"
-        echo "[38;5;255;255;255m#standwithukraine [0m[38;5;7m  $(printf "%-61s" "${description_2}") [0m"
-        echo '[38;5;255;255;255m                          [0m[38;5;7m                                             [0m'
+        echo '[38;5;255;255;255m        [0m[38;5;7m                                                                 [0m'
+        echo "[38;5;196;48;5;196m       [0m[38;5;255;255;255m   $(printf "%-61s" "${title}") [0m"
+        echo "[38;5;196;48;5;196m       [0m[38;5;7m   $(printf "%-61s" "${description_1}") [0m"
+        echo "[38;5;196;48;5;196m       [0m[38;5;7m   $(printf "%-61s" "${description_2}") [0m"
+        echo '[38;5;255;255;255m        [0m[38;5;7m                                                                 [0m'
     fi
 }
